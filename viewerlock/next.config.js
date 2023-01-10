d9a5ae1cb4e67c1246d9ae8a827ce7519deaa08f
Baek, Seunghwa <soarchma@gmail.com>
2023-01-10 10:50:54 +0900

    디렉토리 구조 변경. 어플리케이션 고커라이징.

--- viewerlock/next.config.js
+++ viewerlock/next.config.js
@@ -13,6 +13,13 @@ module.exports = {
       },
     ];
   },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     if (isServer) {
       // return merge(config, {
